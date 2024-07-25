d471eb9c9c230cdac28fd5a72920ccb0904665fc
MikIsCoding <157822670+MikIsCoding@users.noreply.github.com>
2024-07-25 15:11:38 +0200

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,15 +1,19 @@
 -- downloader.lua made by scihb, do NOT remove this comment
---variables
+--argument
 pkgname = arg[1]
---modules
+--variables
 nms = require("pkgnames")
 extrct = require("extract")
 downloader = require("downloader")
--- if statements
+--if statements
 if nms.test == pkgname then
 	downloader.download("test")
 elseif nms.teeworlds == pkgname then
 	downloader.download("teeworlds-0.7.5-linux_x86_64")
+elseif nms.luaplugin_for_godot == pkgname then
+	downloader.download("luaplugingodot")
 else
 	print("package name: " .. pkgname .. " is not found")
 end
+
+
